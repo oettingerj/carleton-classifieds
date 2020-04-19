b878d29dbbc2ca1a4db37556e621723d10498640
xaviert3947 <xaviert@carleton.edu>
2020-04-19 14:38:58 -0500

    google login in APP.JS

--- classifieds/frontend/src/src/Pages/App.js
+++ classifieds/frontend/src/src/Pages/App.js
@@ -11,29 +11,44 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Account from './Account'
 import CreateListing from './CreateListing'
 import ViewListing from './ViewListing'
-import ReactDOM from 'react-dom';
 import GoogleLogin from 'react-google-login';
+import {Redirect} from 'react-router-dom'
 
 const store = createStore()
+
 const responseGoogle = (response) => {
-  console.log(response);
+  console.log('hello')
+  return <Redirect to={'/create_listing'} />
+}
+
+const responseFail = (response) => {
+  return <Redirect to={Account} />
 }
 
 function App () {
   return (
-    <GoogleLogin
-      clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+
+     <Provider store={store}>
+     <GoogleLogin
+      clientId="118120065426-b4m3d8qog18tvtct6p5atkg0249dmds5.apps.googleusercontent.com"
       buttonText="Login"
       onSuccess={responseGoogle}
-      onFailure={responseGoogle}
+      onFailure={responseFail}
       cookiePolicy={'single_host_origin'}
-  />
+      />
+      <Router>
+        <Container fluid style={{ paddingLeft: 0, paddingRight: 0 }}>
+          <NavBar fixed='top' />
+          <Switch>
+            <Route path='/user' component={Account} />
+            <Route path='/listing/:id' component={ViewListing} />
+            <Route path='/create_listing' component={CreateListing} />
+            <Route path='/' component={Home} />
+          </Switch>
+        </Container>
+      </Router>
+    </Provider>
   )
 }
-
-ReactDOM.render(
-  <App/>
-  ,document.getElementById('root')
-);
 
 export default App
--- classifieds/frontend/src/src/Pages/GoogleLoginPage.js
+++ classifieds/frontend/src/src/Pages/GoogleLoginPage.js
@@ -2,42 +2,20 @@
 import ReactDOM from 'react-dom';
 import GoogleLogin from 'react-google-login';
 import React, { Component } from 'react'
-
+import { Redirect } from "react-router-dom";
 
 const responseGoogle = (response) => {
   console.log(response);
 }
 
-export default class GoogleLoginPage extends Component<Props> {
-  render(){
-    return(
-      <GoogleLogin
-        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
+export default function App() {
+  return (
+    <GoogleLogin
+      clientId="118120065426-b4m3d8qog18tvtct6p5atkg0249dmds5.apps.googleusercontent.com"
+      buttonText="Login"
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}
+      cookiePolicy={'single_host_origin'}
       />
   );
 }
-}
-
-
-/*
-export default class GoogleLoginPage extends Component<Props> {
-  render(){
-    return(
-      <div>
-      <GoogleLogin
-        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-      />
-      </div>
-    )
-
-  }
-}
-*/
