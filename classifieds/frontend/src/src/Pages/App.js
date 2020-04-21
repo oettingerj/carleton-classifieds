46762bbb7a1c34da4901218683de133a08d858fb
xaviert3947 <xaviert@carleton.edu>
2020-04-20 21:05:59 -0500

    Google Login Page

--- classifieds/frontend/src/src/Pages/App.js
+++ classifieds/frontend/src/src/Pages/App.js
@@ -9,41 +9,25 @@ import createStore from '../Redux'
 import { Container } from 'react-bootstrap'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Account from './Account'
+import GoogleLoginPage from './GoogleLoginPage'
 import CreateListing from './CreateListing'
 import ViewListing from './ViewListing'
-import GoogleLogin from 'react-google-login';
 import {Redirect} from 'react-router-dom'
+
 
 const store = createStore()
 
-const responseGoogle = (response) => {
-  console.log('hello')
-  return <Redirect to={'/create_listing'} />
-}
-
-const responseFail = (response) => {
-  return <Redirect to={Account} />
-}
-
 function App () {
   return (
-
-     <Provider store={store}>
-     <GoogleLogin
-      clientId="118120065426-b4m3d8qog18tvtct6p5atkg0249dmds5.apps.googleusercontent.com"
-      buttonText="Login"
-      onSuccess={responseGoogle}
-      onFailure={responseFail}
-      cookiePolicy={'single_host_origin'}
-      />
+    <Provider store={store}>
       <Router>
         <Container fluid style={{ paddingLeft: 0, paddingRight: 0 }}>
-          <NavBar fixed='top' />
           <Switch>
+            <Route path='/' component={GoogleLoginPage} />
+            <Route path='/home' component={Home} />
             <Route path='/user' component={Account} />
             <Route path='/listing/:id' component={ViewListing} />
             <Route path='/create_listing' component={CreateListing} />
-            <Route path='/' component={Home} />
           </Switch>
         </Container>
       </Router>
--- classifieds/frontend/src/src/Pages/GoogleLoginPage.js
+++ classifieds/frontend/src/src/Pages/GoogleLoginPage.js
@@ -3,19 +3,36 @@ import ReactDOM from 'react-dom';
 import GoogleLogin from 'react-google-login';
 import React, { Component } from 'react'
 import { Redirect } from "react-router-dom";
+import { Row, Col, Card, Container, Form} from 'react-bootstrap'
+import {withRouter, Router, Route} from 'react-router'
+import Home from './Home'
 
-const responseGoogle = (response) => {
-  console.log(response);
+const responsePass = (response) => {
+  this.props.history.push('/create_listing')
 }
 
-export default function App() {
-  return (
-    <GoogleLogin
-      clientId="118120065426-b4m3d8qog18tvtct6p5atkg0249dmds5.apps.googleusercontent.com"
-      buttonText="Login"
-      onSuccess={responseGoogle}
-      onFailure={responseGoogle}
-      cookiePolicy={'single_host_origin'}
-      />
-  );
+class GoogleLoginPage extends Component<Props>{
+  render(){
+    return (
+      <Row className='justify-content-md-center'>
+        <Col md={5}>
+          <Card style={{ width: '20rem'}}>
+            <Card.Title>Welcome to Carleton Classifieds!</Card.Title>
+              <GoogleLogin
+              clientId="118120065426-b4m3d8qog18tvtct6p5atkg0249dmds5.apps.googleusercontent.com"
+              buttonText="Login to Continue"
+              onSuccess={responsePass}
+              onFailure={responsePass}
+              cookiePolicy={'single_host_origin'}
+              />
+              <Router history={this.props.history}>
+                <Route path="/home" component={Home} />
+              </Router>
+          </Card>
+        </Col>
+    </Row>
+    );
+  }
 }
+
+export default withRouter(GoogleLoginPage)
