7a4c482f3f58a142f40fd55b8eef251d8b787312
Josh Oettinger <joshkyle98@gmail.com>
2020-05-10 20:47:33 -0500

    Fixed login cookie thing

--- src/Sagas/StartupSagas.js
+++ src/Sagas/StartupSagas.js
@@ -1,15 +1,16 @@
 import ListingActions from '../Redux/ListingRedux'
 import API from '../Services/API'
+import Cookies from 'js-cookie'
 
 // runs on startup
 export function * startup ({ store }) {
   const state = store.getState()
-  if (state.user.isLoggedIn) {
+  if (Cookies.get('loggedIn')) {
     const api = API()
     yield api.getItemListings().then((response) => {
       if (response.ok) {
         store.dispatch(ListingActions.setItemListings(response.data))
       }
-    })
+    }).catch((error) => console.warn(error))
   }
 }
